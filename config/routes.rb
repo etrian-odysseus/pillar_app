a0a4c7fd4f63be8983d54eed19648fe5da4e1bd9
Joe Langford <joseph.langford@gmail.com>
2015-02-15 17:42:46 -0500

    setup db and added initial home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'acme_pages/home'
+  root 'acme_pages#home'
+  #get 'acme_pages/home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
