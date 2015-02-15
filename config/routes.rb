42dbcc6c8dc4bfe7a0d2dfa803218f3a0616baa5
Joe Langford <joseph.langford@gmail.com>
2015-02-15 16:48:54 -0500

    added first controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'acme_pages/home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
